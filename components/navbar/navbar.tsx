98bedb8b5f2b749e83814352b50ab5996e50622e
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-30 23:58:45 +0530

    refac: navbar CSS

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -10,19 +10,19 @@ export default function Navbar() {
   const { activeSection, setActiveSection } = useActiveSectionContext();
 
   return (
-    <header className="z-[999] relative">
-      <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-black border-opacity-40 bg-neutral-800 bg-opacity-80 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.4rem] sm:w-[46.5rem] sm:rounded-full"></motion.div>
+    <header className="relative z-[999]">
+      <motion.div className="fixed left-1/2 top-0 h-[4.5rem] w-full -translate-x-1/2 rounded-none border border-black border-opacity-40 bg-neutral-800 bg-opacity-80 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.4rem] sm:w-[46.5rem] sm:rounded-full"></motion.div>
 
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-neutral-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {navItems.map((link) => (
             <motion.li
-              className="h-3/4 flex items-center justify-center relative"
+              className="relative flex h-3/4 items-center justify-center"
               key={link.hash}
             >
               <Link
                 className={cn(
-                  "flex w-full items-center justify-center px-3 py-3 hover:z-10 hover:text-blue-500 transition",
+                  "flex w-full items-center justify-center px-3 py-3 transition hover:z-10 hover:text-blue-500",
                   { "hover:text-neutral-300": activeSection === link.name },
                   { "text-gray-950": activeSection === link.name }
                 )}
@@ -32,7 +32,7 @@ export default function Navbar() {
               >
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-blue-500 rounded-full absolute inset-0 -z-10"
+                    className="absolute inset-0 -z-10 rounded-full bg-blue-500"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
