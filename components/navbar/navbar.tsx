fcae1d684a36dce51765b20eb3ce89fd405f322a
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-30 22:53:03 +0530

    chore: navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
     <header className="z-[999] relative">
-      <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-black border-opacity-40 bg-neutral-800 bg-opacity-80 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.4rem] sm:w-[47rem] sm:rounded-full"></motion.div>
+      <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-black border-opacity-40 bg-neutral-800 bg-opacity-80 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.4rem] sm:w-[46.5rem] sm:rounded-full"></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-neutral-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
