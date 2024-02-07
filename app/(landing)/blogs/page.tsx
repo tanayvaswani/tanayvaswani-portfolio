e1925914b8d949822da4531d42ce49965023e204
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-08 00:01:43 +0530

    chore: removed blogs page

--- app/(landing)/blogs/page.tsx
+++
@@ -1,9 +0,0 @@
-const BlogsPage = () => {
-    return ( 
-        <div className="flex items-center justify-center h-screen">
-            Blogs
-        </div>
-     );
-}
- 
-export default BlogsPage;
