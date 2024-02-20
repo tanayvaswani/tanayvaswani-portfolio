ea1f674d838201aafb0fba2e2b6bf7e431a61ec7
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-20 22:44:08 +0530

    chore: layout

--- components/hero/hero-section.tsx
+++ components/hero/hero-section.tsx
@@ -19,64 +19,54 @@ const poppins = Roboto({
 
 const HeroSection = () => {
   return (
-    <div className="grid md:grid-cols-7 items-center justify-center md:max-w-screen-xl md:mx-auto px-6">
-      {/* Image for Desktop */}
-      <TanayAvatar />
+    <div className="w-full flex flex-col items-center justify-center h-full md:max-w-screen-xl md:mx-auto px-6">
+      <h1
+        className={cn(
+          "text-2xl md:text-4xl font-semibold md:max-w-screen-md mb-6 md:mb-12",
+          headingFont.className
+        )}
+      >
+        <TextGenerateEffect
+          delay={0.1}
+          textColor={""}
+          words="Software Engineering Intern at Stealth Startup, San Francisco Bay Area."
+        />
+      </h1>
 
-      <div className="w-full flex flex-col justify-center col-span-4 h-full">
-        {/* Image for phone */}
-        <TanayAvatar onDesktop={false} />
-
-        <h1
-          className={cn(
-            "text-2xl md:text-4xl font-semibold md:max-w-screen-sm mb-6 md:mb-12",
-            headingFont.className
-          )}
+      <motion.p
+        className={cn(
+          "text-lg text-neutral-400 md:max-w-screen-md mb-3 md:mb-6",
+          poppins.className
+        )}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "easeOut", duration: 2 }}
+      >
+        👋🏻 Hey, I&apos;m{" "}
+        <span className="text-neutral-200 font-semibold">Tanay Vaswani</span> (
+        <Link
+          href={"https://twitter.com/iTanayVaswani"}
+          className="text-blue-500 font-semibold hover:text-blue-500/90"
+          target="_blank"
         >
-          <TextGenerateEffect
-            delay={0.1}
-            textColor={""}
-            words="Software Engineering Intern at Stealth Startup, San Francisco Bay Area."
-          />
-        </h1>
+          @iTanayVaswani
+        </Link>
+        ) a Full Stack Developer from India. Passionate about Software
+        Development, Cloud Engineering, Backend Systems & Open-Source Softwares.
+      </motion.p>
 
-        <motion.p
-          className={cn(
-            "text-lg text-neutral-400 md:max-w-screen-md mb-3 md:mb-6",
-            poppins.className
-          )}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 2 }}
-        >
-          👋🏻 Hey, I&apos;m{" "}
-          <span className="text-neutral-200 font-semibold">Tanay Vaswani</span>{" "}
-          (
-          <Link
-            href={"https://twitter.com/iTanayVaswani"}
-            className="text-blue-500 font-semibold hover:text-blue-500/90"
-            target="_blank"
-          >
-            @iTanayVaswani
-          </Link>
-          ) a Full Stack Developer from India. Passionate about Software
-          Development, Cloud Engineering, Backend Systems & Open-Source
-          Softwares.
-        </motion.p>
-
-        <motion.p
-          className={cn(
-            "text-lg text-neutral-400 md:max-w-screen-md",
-            poppins.className
-          )}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 2 }}
-        >
-          I&apos;m currently an undergraduate, pursuing Electronics &
-          Communication Engineering majors from New Delhi, India.
-        </motion.p>
-      </div>
+      <motion.p
+        className={cn(
+          "text-lg md:max-w-screen-md text-neutral-400",
+          poppins.className
+        )}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "easeOut", duration: 2 }}
+      >
+        I&apos;m currently an undergraduate, pursuing Electronics &
+        Communication Engineering majors from New Delhi, India.
+      </motion.p>
     </div>
   );
 };
