b6f07ce37bc07b56bee48fccfef46e41d4f52b06
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-22 17:02:45 +0530

    feat: projects card

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
   return (
     <div className="group mb-3 sm:mb-8">
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:min-h-[20rem] group-even:pl-8 hover:bg-gray-200 transition rounded-lg">
+      <section className="bg-slate-950 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:min-h-[20rem] group-even:pl-8 hover:bg-slate-900 transition rounded-lg">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
           <h3 className="text-2xl font-semibold">{title}</h3>
 
@@ -38,7 +38,7 @@ const ProjectCard = ({
           src={imageUrl}
           alt="project-card-image"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl hidden md:block
                     transition
                     group-hover:scale-[1.04]
                     group-hover:-translate-x-3
