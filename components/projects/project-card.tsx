ec8600146b67e04f3393b4f9037588f036b4e14b
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-22 19:23:57 +0530

    fix: project card

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -14,18 +14,18 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
   return (
     <div className="group mb-3 sm:mb-8">
-      <section className="bg-slate-950 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:min-h-[20rem] group-even:pl-8 hover:bg-slate-900 transition rounded-lg">
+      <section className="bg-neutral-200 max-w-[48rem] overflow-hidden sm:pr-8 relative sm:min-h-[20rem] group-even:pl-8 hover:bg-neutral-100 transition rounded-lg">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
-          <h3 className="text-2xl font-semibold">{title}</h3>
+          <h3 className="text-2xl font-semibold text-neutral-800">{title}</h3>
 
-          <p className="mt-2 text-xs leading-relaxed text-gray-700">
+          <p className="mt-2 mb-2 text-sm leading-relaxed text-gray-700">
             {description}
           </p>
 
           <ul className="flex flex-wrap gap-2 mt-auto">
             {tags.map((tag, index) => (
               <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                className="bg-black/[0.7] px-3 py-1 text-[0.6rem] uppercase tracking-wider text-white rounded-full"
                 key={index}
               >
                 {tag}
