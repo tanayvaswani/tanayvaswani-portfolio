c175ce1c0a8e228086ddf973dfb9ee53170a62a3
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-19 00:17:39 +0530

    feat: experience content

---
+++ components/experience/experience-content.ts
@@ -0,0 +1,20 @@
+export type experienceContent = {
+  title: string;
+  company: string;
+  duration: string;
+  description: string[];
+};
+
+export const experienceContent = [
+  {
+    title: "Software Engineering Intern",
+    company: "Stealth Startup",
+    duration: "November 2023 - Present",
+    description: [
+      "Spearheaded a team of Software Developers to develop a new website and platform UI for our flagship product, resulting in a 150% increase in user engagement",
+      "Designed and implemented a scalable backend infrastructure to self-host an open-source CMS using cloud technologies, leading to a 20% reduction in server costs and improved system performance during peak traffic hours.",
+      "Implemented CI/CD pipeline for hosting, resulting in a 50% decrease in bugs and website downtime, hence improving overall product reliability.",
+      "Worked closely with founders to translate business requirements into technical specifications, ensuring alignment between development efforts and product objectives.",
+    ],
+  },
+];
