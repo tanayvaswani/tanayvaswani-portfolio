6ce800c02a218314d6195e1bf780b57ac87bb728
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-18 14:38:56 +0530

    feat: project card

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import React from "react";
+import {
+  ExternalLink,
+  GanttChartIcon,
+  GithubIcon,
+  YoutubeIcon,
+} from "lucide-react";
+import Link from "next/link";
+
 import type { projectData } from "./project-content";
 
 type ProjectCardProps = projectData;
@@ -10,19 +18,33 @@ const ProjectCard = ({ title, description, tags }: ProjectCardProps) => {
     <div className="md:max-w-screen-md md:mx-auto w-full py-12 px-6 border-b">
       <section className="w-full flex flex-col items-start justify-center">
         <div className="flex flex-col items-start justify-center">
-          <h3 className="text-xl md:text-2xl font-semibold text-neutral-300">
-            {title}
+          <h3 className="text-xl md:text-2xl font-semibold text-neutral-300 flex items-center justify-between w-full">
+            <div className="flex items-center">
+              <GanttChartIcon className="mr-2" /> {title}
+            </div>
+
+            <div className="flex items-center gap-4 ml-4">
+              <Link href={link.github}>
+                <GithubIcon className="h-5 w-5" />
+              </Link>
+              <Link href={link.youtube}>
+                <YoutubeIcon className="h-5 w-5" />
+              </Link>
+              <Link href={link.live}>
+                <ExternalLink className="h-5 w-5" />
+              </Link>
+            </div>
           </h3>
 
-          <p className="my-2 text-neutral-400 text-sm md:text-lg">
+          <p className="my-2 text-neutral-400 text-base md:text-lg px-3">
             {description}
           </p>
         </div>
 
-        <div className="flex flex-wrap items-start justify-start gap-2 text-xs md:text-sm">
+        <div className="flex flex-wrap items-start justify-start gap-2 text-xs md:text-sm pt-4 px-2">
           {tags.map((tag, index) => (
             <div
-              className="bg-neutral-300/60 text-neutral-800 px-2 py-1 rounded-full shadow-md"
+              className="text-blue-300 px-2 md:px-4 py-1 rounded-full shadow-sm border"
               key={index}
             >
               {tag}
