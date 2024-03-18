7c1bde0d8cea43b990a54e97f55dcb76da82aaf2
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-19 00:04:49 +0530

    feat: exp

--- components/experience/experience-section.tsx
+++ components/experience/experience-section.tsx
@@ -14,14 +14,14 @@ const ExperienceSection = () => {
         <div className="flex flex-col items-start justify-between w-full">
           <div className={cn("flex flex-col", headingFont.className)}>
             <h1 className="flex items-center text-xl md:text-2xl text-neutral-300">
-              <GanttChart className="mr-2" /> Software Engineering Intern,
-              Stealth Startup
+              <GanttChart className="mr-1 hidden md:block" /> Software
+              Engineering Intern, Stealth Startup
             </h1>
           </div>
 
           <div
             className={cn(
-              "flex items-center justify-between w-full md:px-6 px-0",
+              "flex flex-col md:flex-row items-start md:items-center justify-between w-full md:px-8 px-0",
               headingFont.className
             )}
           >
@@ -36,8 +36,8 @@ const ExperienceSection = () => {
         </div>
       </div>
 
-      <div>
-        <p>Description</p>
+      <div className="px-6">
+        <p>{}</p>
       </div>
     </div>
   );
