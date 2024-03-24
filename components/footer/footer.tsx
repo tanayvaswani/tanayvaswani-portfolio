96e473a616574155f0bb2d7e608ee0418684a6a4
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-24 14:01:18 +0530

    fix: footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,15 +3,15 @@ import { Button } from "../ui/button";
 
 const Footer = () => {
   return (
-    <div className="border-t w-full px-6 py-2 text-zinc-500">
-      <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full">
+    <div className="border-t w-full px-6 py-4 text-zinc-500">
+      <div className="flex flex-col-reverse md:flex-row-reverse items-center justify-between w-full">
         <div className="flex flex-row items-center justify-between space-x-4">
-          <Button size={"sm"} variant={"ghost"} asChild>
+          <Button size={"sm"} variant={"link2"} asChild>
             <Link href={"https://github.com/tanayvaswani"} target="_blank">
               GitHub
             </Link>
           </Button>
-          <Button size={"sm"} variant={"ghost"} asChild>
+          <Button size={"sm"} variant={"link2"} asChild>
             <Link href={"https://twitter.com/iTanayVaswani"} target="_blank">
               Twitter/X
             </Link>
