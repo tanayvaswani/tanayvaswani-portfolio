c9b04c68c7d39cfee40e1b43028a09af578b58b7
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-08 11:00:22 +0530

    refac: project section

--- app/(landing)/projects/page.tsx
+++ app/(landing)/projects/page.tsx
@@ -1,9 +1,22 @@
-import ProjectSection from "@/components/projects/project-section";
+import { projectsContent } from "@/components/projects/project-content";
+import ProjectCard from "@/components/projects/project-card";
 
 const ProjectsPage = () => {
   return (
-    <div className="flex items-center justify-center">
-      <ProjectSection />
+    <div className="my-8 md:my-20 w-full md:max-w-screen-2xl md:mx-auto md:px-16">
+      <ul className="flex flex-col items-center justify-center w-full">
+        {projectsContent.map((card, idx) => {
+          return (
+            <ProjectCard
+              title={card.title}
+              description={card.description}
+              tags={card.tags}
+              key={idx}
+              link={card.link}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 };
